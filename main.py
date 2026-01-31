84d8aa7dc53b54247f6d28107ee35cc6818a57a3
Igor <korneyko2607@gmail.com>
2026-01-31 19:44:49 +0200

    basic func

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QPushButton, QLabel, QLayout
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QApplication,
+                              QPushButton, QLabel, QFrame, QTabWidget, QLineEdit,
+                              QCheckBox, QVBoxLayout, QHBoxLayout, QScrollArea)
 import sys
 
 
 class Job_seeker_app(QMainWindow):
-    pass
+    def __init__(self):
+        super().__init__()
+        self._create_widgets()
+        self._initUI()
+        self._set_layouts()
+
+        
+    def _create_widgets(self):
+        #global thing
+        self.tabs = QTabWidget()
+        self.setCentralWidget(self.tabs)
+
+        #main page widgets
+        self.main_page = QWidget()#главній єкра с поиском и тд
+        self.search_bar = QLineEdit()
+        self.add_filter_button = QPushButton()
+        self.delete_filter_button = QPushButton()
+        self.filters_area = QScrollArea()
+        self.search_job_button = QPushButton()
+        self.work_ua_box = QCheckBox()
+        self.robota_ua_box = QCheckBox()
+        self.founded_jobs_area = QScrollArea()
+
+        #saved jobs list widgets
+        self.save_list_page = QWidget()#страница со списком сохраненіїх раьбот
+        self.job_list_area = QScrollArea()
+
+    def _initUI(self):
+        self.setWindowTitle('Job Seeker')
+
+    def _set_layouts(self):
+        pass
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
