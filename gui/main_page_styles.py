dbb4a16824df65a5cca00292f2082a80f863a93c
Igor <korneyko2607@gmail.com>
2026-02-02 19:10:00 +0200

    styles to search job button

--- gui/main_page_styles.py
+++ gui/main_page_styles.py
@@ -1,7 +1,8 @@
+#styles to add filter button
 
 def style_add_filter_button():
     return """QPushButton#add_filter_button{
-        font-size: 15px;
+        font-size: 35px;
         font-family: Calibri;
         background-color:hsl(131, 96%, 44%);
         color: white;
@@ -16,5 +17,38 @@ def style_add_filter_button():
         }
         QPushButton#add_filter_button:pressed {
             background-color: hsl(131, 100%, 27%);
+            color:white;
         }
         """
+
+#styles to search job button
+
+def style_search_job_button():
+    return """QPushButton#search_job_button{
+        font-size: 35px;
+        font-family: Calibri;
+        background-color:hsl(4, 72%, 53%);
+        color: white;
+        border-radius: 15px;
+        padding:5ps;
+        border: none;
+        }
+        QPushButton#search_job_button:hover{
+            color: hsl(0, 70%, 63%);
+            background-color: white;
+        }
+        QPushButton#search_job_button:pressed {
+            background-color: hsl(0, 75%, 31%);
+            color:white;
+        }
+
+    """
+
+#styles to searchbar
+
+def style_searchbar():
+    return """QLineEdit{
+        font-size: 20px;
+    }
+    """
+
