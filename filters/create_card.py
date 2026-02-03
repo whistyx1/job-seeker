035ce12256be0eb889b75af759af1281ee1e95af
Igor <korneyko2607@gmail.com>
2026-02-03 22:16:01 +0200

    add hover cursor

--- filters/create_card.py
+++ filters/create_card.py
@@ -1,5 +1,6 @@
 from .get_filters import get_filters
 from PyQt5.QtWidgets import QMessageBox, QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout
+from PyQt5.QtCore import Qt
 from .delete_filters import delete_filter
 from gui.style_filter_card import style_filter_card
 
@@ -12,6 +13,7 @@ def create_filters_card(text: str):
 
     delete_button = QPushButton()
     delete_button.setText('Delete')
+    delete_button.setCursor(Qt.PointingHandCursor)
 
     layout.addWidget(text_label)
     layout.addWidget(delete_button)
