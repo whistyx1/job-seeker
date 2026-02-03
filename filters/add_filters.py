8363f7ff28850f3a95b4e7735decc87229edde3c
Igor <korneyko2607@gmail.com>
2026-02-03 22:06:01 +0200

    fixed problem with incorrect deleting

--- filters/add_filters.py
+++ filters/add_filters.py
@@ -2,19 +2,20 @@ from .create_card import create_filters_card
 from .get_filters import get_filters
 from .delete_filters import delete_filter
 from PyQt5.QtWidgets import QMessageBox, QPushButton    
+from functools import partial
 
 def add_filter(searchbar, layout):
     filters = get_filters(searchbar)
 
     if not filters:
-        QMessageBox.warning(None, title='error', text='An Empty Bar can not be a Filter\nTry again')
+        QMessageBox.warning(None, 'error', 'An Empty Bar can not be a Filter\nTry again')
         return
     else:
         for text in filters:
             card = create_filters_card(text)
 
             delete_button = card.findChild(QPushButton)
-            delete_button.clicked.connect(lambda checked: delete_filter(card, layout))
+            delete_button.clicked.connect(partial(delete_filter, card, layout))
 
             layout.addWidget(card)
     
