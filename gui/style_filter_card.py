70fdc6662206f974b8ba78d9fa6c2381451e89ac
Igor <korneyko2607@gmail.com>
2026-02-03 21:57:11 +0200

    styles for filter card

---
+++ gui/style_filter_card.py
@@ -0,0 +1,18 @@
+def style_filter_card():
+    return """QLabel{
+        font-size: 15px;
+        font-family: Calibri;
+        color: #0c0033;
+    }QPushButton{
+        font-size:15px;
+        padding:5px;
+        border-radius:10px;
+        border:none;
+        background-color:#ff0000
+        color: white;
+    }QPushButton:hover {
+        color: red;
+        background-color: white;
+    }
+
+    """
