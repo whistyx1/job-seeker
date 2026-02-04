32f8b253b50d601630e60054a4ebe4edf699a062
Igor <korneyko2607@gmail.com>
2026-02-04 22:36:26 +0200

    fix adding empty state

--- filters/delete_filters.py
+++ filters/delete_filters.py
@@ -1,6 +1,15 @@
 def delete_filter(card, layout, empty_state_widget):
     layout.removeWidget(card)
+    card.hide()
     card.deleteLater()
 
-    if layout.count() == 0:
-        empty_state_widget.show()
+    has_cards = False
+    for i in range(layout.count()):
+        widget = layout.itemAt(i).widget()
+        if widget and widget != empty_state_widget and widget.isVisible():
+            has_cards = True
+            break
+
+    if not has_cards:
+        if empty_state_widget:
+            empty_state_widget.show()
