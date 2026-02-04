afd10847febc0e1efbf7adb707973b2bd1713c20
Igor <korneyko2607@gmail.com>
2026-02-04 09:32:12 +0200

    add widget styles

--- gui/style_filter_card.py
+++ gui/style_filter_card.py
@@ -1,9 +1,15 @@
 def style_filter_card():
     return """
+        QWidget{
+        background-color:#21eb72;
+        border-radius: 5px;
+        border: 1px solid #061c0f;
+        }
         QLabel{
             font-size: 25px;
             font-family: Calibri;
-            color: #0c0033;
+            color: #13291c;
+            border:none;
         }QPushButton{
             font-size:20px;
             padding:5px;
