5ee084f19fec8cf262e3a6f27e306ee25dca62c1
Igor <korneyko2607@gmail.com>
2026-02-08 13:02:48 +0200

    fix missing error

--- jobs/save_card.py
+++ jobs/save_card.py
@@ -1,9 +1,10 @@
 from PyQt5.QtWidgets import QMessageBox
 from .display_saved_jobs import display_saved_jobs
 
-def save_job_card(job, job_list):
+def save_job_card(job, job_list, parent=None):
     if job.url in [j.url for j in job_list]:
         QMessageBox.warning(
+            parent,
             'Already exist', 
             'You have already saved this job'
         )
